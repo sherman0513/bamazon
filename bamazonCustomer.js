23a8a99e9e36173fc3e26968a7a96ea1b70ca019
unknown <sherman.jonathan.r@gmail.com>
2019-05-05 23:05:25 -0700

    sql table now updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,11 +2,11 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-	host:"localhost",
-	port:3306,
-	user:"root",
-	password:"password",
-	database:"bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon"
 });
 
 function stockedItems() {
@@ -14,7 +14,7 @@ function stockedItems() {
     let items = [];
     // let stock = [];
 
-    connection.query('select * from products', function(err, results) {
+    connection.query('select * from products', function (err, results) {
         if (err) {
             console.log(err);
             return;
@@ -40,19 +40,48 @@ function stockedItems() {
         ]).then(purchased => {
             // console.log(purchased.select, purchased.howMany);
             let amount = purchased.howMany;
-            console.log('amount: ', amount);
+            // console.log('amount: ', amount);
             let chosenItem;
             items.forEach(result => {
                 // console.log(result);
                 if (result.id === parseInt(purchased.select)) {
-                    console.log('result.select: ', purchased.select);
-                    console.log('result.id: ', result.id);
+                    // console.log('result.select: ', purchased.select);
+                    // console.log('result.id: ', result.id);
                     chosenItem = result.id;
-                    console.log(chosenItem);
+                    // console.log(chosenItem);
                 };
             });
+            let needed = amount;
+            let IDreq = chosenItem;
+            finalizeOrder(IDreq, needed);
         });
     });
+    
 };
+
+function finalizeOrder(IDreq, needed) {
+    connection.query('select * from products where item_id = ?', [IDreq], function (err, result) {
+        if (err) {
+            console.log(err);
+        };
+        if (needed <= result[0].stock_quantity) {
+            let totalCost = result[0].price * needed;
+            console.log('Good news your order is in stock!');
+            console.log('Your total cost for ' + needed + ' ' + result[0].product_name + ' is ' + totalCost + ' Thank you!');
+            let newStock = (result[0].stock_quantity - needed);
+            // console.log('newStock: ', newStock);
+
+            connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: newStock
+            }, {
+                item_id: IDreq
+            }])
+        } else {
+            console.log('Insufficient quantity, sorry we do not have enough ' + result[0].product_name + ' to complete your order.');
+        };
+        connection.end();
+    })   
+
+}
 
 stockedItems();
